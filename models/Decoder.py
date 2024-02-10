118379c63946f8843a98d405f5ffdd238cbe9090
LiteShake <strawberryshakelite@gmail.com>
2024-02-10 21:27:51 +0530

    edit encoder decoder code

--- models/Decoder.py
+++ models/Decoder.py
@@ -1,9 +1,9 @@
 
 import torch
 import torch.nn as nn
-from Attention import *
-from PosEncoding import *
-import torch.functional as F
+from .Attention import *
+from .PosEncoding import *
+import torch.nn.functional as F
 
 class Decoder(nn.Module):
     
@@ -16,24 +16,29 @@ class Decoder(nn.Module):
         self.sa_1 = SDPA(in_size)
         self.sa_2 = SDPA(in_size)
         self.sa_3 = SDPA(in_size)
-        self.att_linear_1 = nn.Linear(in_size)
+        
+        self.att_linear_1 = nn.Linear(3 * in_size, in_size)
         
         self.FF1 = nn.Linear(in_size, in_size)
         self.FF2 = nn.Linear(in_size, in_size)
         
         self.Linear1 = nn.Linear(in_size, in_size)
         
-    def forward(self, x, encoder_in, pos):
+        self.layernorm1 = nn.LayerNorm(in_size)
+        self.layernorm2 = nn.LayerNorm(in_size)
+        self.layernorm3 = nn.LayerNorm(in_size)
         
-        pos_vector = GetPositionalEncoding(pos, x.shape, x.shape)
+    def forward(self, x, encoder_in):
         
-        positioned_input = x + pos_vector
+        # pos_vector = GetPositionalEncoding(pos, x.shape, x.shape)
+        
+        # positioned_input = x + pos_vector
         
         # region SELF ATTENTION + NORM
-        att_1 = self.mha1(positioned_input)
+        att_1 = self.mha1(x)
         
         residual_1 = x + att_1
-        normalized_1 = nn.LayerNorm(residual_1)
+        normalized_1 = self.layernorm1(residual_1)
         # endregion
         
         # region CROSS ATTENTION
@@ -42,17 +47,18 @@ class Decoder(nn.Module):
         head3 = self.sa_3(normalized_1)
         # endregion
         
-        cat_1 = torch.cat(head1, head2, head3)
-        att_2 = self.att_linear_1(cat_1)
+        cat_1 = torch.cat((head1, head2, head3), 0)
+        flt = torch.flatten(cat_1)
+        att_2 = self.att_linear_1(flt)
         
         residual_2 = att_2 + normalized_1
-        normalized_2 = nn.LayerNorm(residual_2)
+        normalized_2 = self.layernorm2(residual_2)
         
         dense_1 = F.relu(self.FF1(normalized_2))
         dense_2 = F.relu(self.FF2(dense_1))
         
         residual_3 = residual_1 + dense_2
-        normalized_3 = nn.LayerNorm(residual_3)
+        normalized_3 = self.layernorm3(residual_3)
         
         # region CLASSIFIER
         """ 
--- models/Encoder.py
+++ models/Encoder.py
@@ -1,9 +1,8 @@
-
 import torch
 import torch.nn as nn
-from Attention import *
-from PosEncoding import *
-import torch.functional as F
+from .Attention import *
+from .PosEncoding import *
+import torch.nn.functional as F
 
 class Encoder(nn.Module):
     
@@ -13,22 +12,24 @@ class Encoder(nn.Module):
         self.mha1 = MHA(in_size)
         self.FF1 = nn.Linear(in_size, in_size)
         self.FF2 = nn.Linear(in_size, in_size)
+        self.layernorm1 = nn.LayerNorm(in_size)
+        self.layernorm2 = nn.LayerNorm(in_size)
         
-    def forward(self, x, pos):
+    def forward(self, x):
         
-        pos_vector = GetPositionalEncoding(pos, x.shape, x.shape)
+        # pos_vector = GetPositionalEncoding(pos, x.shape, x.shape)
         
-        positioned_input = x + pos_vector
+        # positioned_input = x + pos_vector
         
-        att_vals = self.mha1(positioned_input)
+        att_vals = self.mha1(x)
         
         residual_1 = x + att_vals
-        normalized_1 = nn.LayerNorm(residual_1)
+        normalized_1 = self.layernorm1(residual_1)
         
         dense_1 = F.relu(self.FF1(normalized_1))
         dense_2 = F.relu(self.FF2(dense_1))
         
         residual_2 = residual_1 + dense_2
-        normalized_2 = nn.LayerNorm(residual_2)
+        normalized_2 = self.layernorm2(residual_2)
         
         return normalized_2
